996550b3c8bc2f719f2b5f3c8ab38bb504b48f2c
Hans <chiuhans111@gmail.com>
2022-06-24 09:50:46 +0800

    Initial Test

---
+++ src/App.vue
@@ -0,0 +1,12 @@
+<script setup>
+import TestItem from "./components/test-item.vue";
+</script>
+
+<template>
+  <div>
+    <h1>FDTD simulator</h1>
+    <TestItem></TestItem>
+  </div>
+</template>
+
+<style></style>
